458a2e78112e5171f1fdc42bab02a577c109797f
kolamat <matanmidupe004@gmail.com>
2024-08-07 13:03:23 +0100

    On branch main
    Initial commit
    
    Changes to be committed:
    	new file:   index.html
    	new file:   index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// var popPin = alert("Bro Olumide pls employ me");
+
+let body = document.querySelector("body");
+body.style.fontStyle = "italic";
+
+const heading = document.createElement("h1");
+heading.textContent =
+  "My Name is 'Olumide' I will employ you but how is your monthly pay";
+document.querySelector("body").appendChild(heading);
+const heading1 = document.createElement("h1");
+heading1.textContent =
+  "My Name is 'Kolade' So How Much Will You Pay Bro Olumide";
+document.querySelector("body").appendChild(heading1);
