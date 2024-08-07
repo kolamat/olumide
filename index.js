779be73591d29af1aca9c7b7530c7f2aa7ebdec9
kolamat <matanmidupe004@gmail.com>
2024-08-07 13:43:02 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -2,12 +2,31 @@
 
 let body = document.querySelector("body");
 body.style.fontStyle = "italic";
+body.style.margin = "0px";
+body.style.padding = "0px";
+body.style.display = "flex";
+body.style.justifyContent = "space-between";
 
-const heading = document.createElement("h1");
-heading.textContent =
-  "My Name is 'Olumide' I will employ you but how is your monthly pay";
-document.querySelector("body").appendChild(heading);
-const heading1 = document.createElement("h1");
-heading1.textContent =
-  "My Name is 'Kolade' So How Much Will You Pay Bro Olumide";
-document.querySelector("body").appendChild(heading1);
+const button = document.createElement("button");
+button.id = "butt";
+button.onclick = showWords;
+button.textContent = "Click_Me";
+document.querySelector("body").appendChild(button);
+
+const button1 = document.createElement("button");
+button1.onclick = closeWords;
+button1.textContent = "Click_Me_Too";
+document.getElementById("butt").appendChild(button1);
+
+function showWords() {
+  let heading = document.createElement("h1");
+  heading.textContent =
+    "My Name is 'Olumide' I will employ you but how is your monthly pay";
+  document.querySelector("button").appendChild(heading);
+}
+
+function closeWords() {
+  let closeHeading = document.createElement("p");
+  closeHeading.textContent = "**********";
+  //   document.querySelector("button").appendChild(closeHeading);
+}
